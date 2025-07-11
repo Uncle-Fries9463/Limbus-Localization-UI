c0bcde86adc36bfd80b5f646f62a7366e0138c5d
x1b[38;5;147m <x1bviolet@gmail.com>
2025-07-11 23:44:58 +0500

    Update ScansManager.cs

--- ScansManager.cs
+++ ScansManager.cs
@@ -15,50 +15,53 @@ namespace LC_Localization_Task_Absolute
     {
         internal protected static void ToggleScanAreaView()
         {
-            if (MainControl.SkillsPreviewFreeBordersCanvas.ActualHeight > 0)
+            if (Mode_Handlers.Upstairs.ActiveProperties.Key == "Skills")
             {
-                if (MainControl.ScanAreaView_Skills.BorderThickness.Top == 0)
+                if (MainControl.SkillsPreviewFreeBordersCanvas.ActualHeight > 0)
                 {
-                    // ACTIVATE
-                    MainControl.SurfaceScrollPreview_Skills.ReconnectAsChildTo(MainControl.SkillsPreviewFreeBordersCanvas);
-                    try
+                    if (MainControl.ScanAreaView_Skills.BorderThickness.Top == 0)
                     {
-                        MainControl.SurfaceScrollPreview_Skills.Height = MainControl.SkillsPreviewFreeBordersCanvas.ActualHeight - 60;
-                    }
-                    catch { }
+                        // ACTIVATE
+                        MainControl.SurfaceScrollPreview_Skills.ReconnectAsChildTo(MainControl.SkillsPreviewFreeBordersCanvas);
+                        try
+                        {
+                            MainControl.SurfaceScrollPreview_Skills.Height = MainControl.SkillsPreviewFreeBordersCanvas.ActualHeight - 60;
+                        }
+                        catch { }
                     
-                    if (Configurazione.DeltaConfig.ScanParameters.AreaWidth != 0)
-                    {
-                        MainControl.PreviewLayoutGrid_Skills_ContentControlStackPanel.Width = Configurazione.DeltaConfig.ScanParameters.AreaWidth;
+                        if (Configurazione.DeltaConfig.ScanParameters.AreaWidth != 0)
+                        {
+                            MainControl.PreviewLayoutGrid_Skills_ContentControlStackPanel.Width = Configurazione.DeltaConfig.ScanParameters.AreaWidth;
+                        }
+                        else
+                        {
+                            MainControl.PreviewLayoutGrid_Skills_ContentControlStackPanel.Width = Mode_Skills.LastRegisteredWidth;
+                        }
+                    
+                        MainControl.ScanAreaView_Skills.BorderThickness = new Thickness(2);
+                        MainControl.ScanAreaView_Skills.Background = ToColor("#E1121212");
+
+                        MainControl.PreviewScanButtonIndicator.Foreground = ToColor("#FF383838");
+                        MainControl.MakeLimbusPreviewScan.IsHitTestVisible = false;
+
+                        SettingsWindow.SettingsControl.ToggleScansPreview_I.Visibility = Visible;
                     }
                     else
                     {
+                        // HIDE
+                        MainControl.SurfaceScrollPreview_Skills.ReconnectAsChildTo(MainControl.PreviewLayoutGrid_Skills);
+                        MainControl.SurfaceScrollPreview_Skills.Height = double.NaN;
+
                         MainControl.PreviewLayoutGrid_Skills_ContentControlStackPanel.Width = Mode_Skills.LastRegisteredWidth;
+
+                        MainControl.ScanAreaView_Skills.BorderThickness = new Thickness(0);
+                        MainControl.ScanAreaView_Skills.Background = ToColor("#00121212");
+
+                        MainControl.PreviewScanButtonIndicator.Foreground = ToColor("#FF9D9D9D");
+                        MainControl.MakeLimbusPreviewScan.IsHitTestVisible = true;
+
+                        SettingsWindow.SettingsControl.ToggleScansPreview_I.Visibility = Collapsed;
                     }
-                    
-                    MainControl.ScanAreaView_Skills.BorderThickness = new Thickness(2);
-                    MainControl.ScanAreaView_Skills.Background = ToColor("#E1121212");
-
-                    MainControl.PreviewScanButtonIndicator.Foreground = ToColor("#FF383838");
-                    MainControl.MakeLimbusPreviewScan.IsHitTestVisible = false;
-
-                    SettingsWindow.SettingsControl.ToggleScansPreview_I.Visibility = Visible;
-                }
-                else
-                {
-                    // HIDE
-                    MainControl.SurfaceScrollPreview_Skills.ReconnectAsChildTo(MainControl.PreviewLayoutGrid_Skills);
-                    MainControl.SurfaceScrollPreview_Skills.Height = double.NaN;
-
-                    MainControl.PreviewLayoutGrid_Skills_ContentControlStackPanel.Width = Mode_Skills.LastRegisteredWidth;
-
-                    MainControl.ScanAreaView_Skills.BorderThickness = new Thickness(0);
-                    MainControl.ScanAreaView_Skills.Background = ToColor("#00121212");
-
-                    MainControl.PreviewScanButtonIndicator.Foreground = ToColor("#FF9D9D9D");
-                    MainControl.MakeLimbusPreviewScan.IsHitTestVisible = true;
-
-                    SettingsWindow.SettingsControl.ToggleScansPreview_I.Visibility = Collapsed;
                 }
             }
         }
